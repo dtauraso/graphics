79f2dbed2c7f67525b87ff4035d6137d03efd4b2
David Tauraso <david318@me.com>
2020-07-07 21:36:25 -0700

    Added code for the vector

--- ternary_search_tree.py
+++ ternary_search_tree.py
@@ -4,8 +4,81 @@
 
 hash_table_trie = {}
 
+# we don't need a copy of the methods for each vector used in the trie tree
+class Vector():
+    def __init__(self):
+        self.container = []
+        self.end = 0
+    def __len__(self):
+        return len(self.container)
+    def last(self):
+        return self.container[-1]
+    def __getitem__(self, key):
+        return self.container[key]
+    def __setitem_(self, key, value):
+        self.container[key] = value
+    def append(self, value):
+        self.container.append(value)
+    def drop(self, n):
+        return self.container[n:]
+    def get_in_range(self, a, b):
+        return self.container[a:b]
 
+def appendItem(vector, item):
+    if len(vector) == 0:
+        vector.append(item)
+        vector.end = 1
+        return vector
+    else:
+        # last item has data
+        if len(vector.last()) > 0:
+            # double size of array
+            size = len(vector)
+            for i in range(size):
+                vector.append({})
+
+        vector[end] = item
+        vector.end += 1
+
+        return vector
+
+def deleteItem(vector, i):
+    if len(vector) == 0:
+        return vector
+    else:
+        new_array = []
+        if i == 0:
+            new_array = vector.drop(i + 1)
+        else:
+            new_array = vector.get_in_range(0, i) + vector.drop(i + 1)
+        vector.container = new_array
+        vector.end -= 1
+
+
+def findLastParent(trie):
+    # last parent whose children haven't been filled up yet
+    # it needs to return the parent reference
+    print(trie)
+    edges = trie['children']
+    parent = trie
+    while len(edges) >= 93:
+        # fails in this loop
+        parent = edges
+        edges = trie[len(parent) - 1]['children']
+    
+    children = edges
+    
+    if len(children) == 0:
+        # print({'parent': parent, 'next_child': '!'})
+        return {'parent': parent, 'next_child': '!'}
+    else:
+        next_edge = chr(ord(list(children)[-1]) + 1) 
+
+        # print({'parent': parent, 'next_child': next_edge})
+        return {'parent': parent, 'next_child': next_edge}
+    
 # use hash table for now
+# replace with vector
 def insert(trie, string):
 
     trie_tracker = trie
@@ -24,6 +97,14 @@ def insert(trie, string):
                 if not new_nodes_made:
                     print('same item twice', trie_tracker)
                     # locate last parent whose children haven't been filled up yet
+                    last_parent_and_child = findLastParent(trie_tracker[char])
+                    print(last_parent_and_child)
+                    parent = last_parent_and_child['parent']
+                    child = last_parent_and_child['next_child']
+                    print('pair', child, parent)
+                    parent['children'][child] = { 'children': {},
+                                                                'end_of_word': True,
+                                                                'state': 0}
 
         trie_tracker = trie_tracker[char]['children']
     
